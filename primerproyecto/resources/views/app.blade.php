a65d9081d55a4ef121f1723af784213260976e19
Erick Barrera <sistemas45@sanmartinbakery.com>
2023-10-13 11:15:02 -0600

    se creo el proyecto base para probar laravel

---
+++ primerproyecto/resources/views/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esta es mi proyecto 1 </title>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Mis tareas</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">TAREAS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">CATEGORIAS</a>
+        </li>
+      </ul>
+     
+    </div>
+  </div>
+</nav>
+
+    @yield('content')
+</body>
+</html>
---
+++ primerproyecto/resources/views/todos/index.blade.php
@@ -0,0 +1,15 @@
+@extends('app')
+
+@section('content')
+ <div class="container w-25 border p-4">
+    <form>
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Titulo de la tarea</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <br>
+  <button type="submit" class="btn btn-primary">Crear tareas</button>
+</form>
+    </form>
+ </div>
+@endsection
